35880944d124ca15d2c9c8a1454e1534f428b49c
piponaka-ku <pipo.nakaku49.nk@gmail.com>
2021-05-16 17:25:48 +0900

    動かす方向を変更

--- ShaderGraph2D/Assets/Script/TransitionPostEffect.cs
+++ ShaderGraph2D/Assets/Script/TransitionPostEffect.cs
@@ -8,7 +8,7 @@ public class TransitionPostEffect : MonoBehaviour
     private Material postEffectMaterial;
 
     [SerializeField]
-    private float transitionTime = 1f;
+    private float transitionTime;
 
     private readonly int _progress = Shader.PropertyToID("_Progress");
 
@@ -16,7 +16,7 @@ public class TransitionPostEffect : MonoBehaviour
     {
         if (postEffectMaterial != null)
         {
-            StartCoroutine(Transition());
+            StartCoroutine(Transition1());
         }
     }
 
@@ -31,5 +31,22 @@ public class TransitionPostEffect : MonoBehaviour
             t += Time.deltaTime;
         }
         postEffectMaterial.SetFloat(_progress, 1f);
+    }
+
+    private IEnumerator Transition1()
+    {
+        float t = -1f;
+        while (t < transitionTime)
+        {
+            float progess = t / transitionTime;
+            if (progess >= 0f)
+            {
+                break;
+            }
+            postEffectMaterial.SetFloat(_progress, progess);
+            yield return null;
+            t += Time.deltaTime;
+        }
+        postEffectMaterial.SetFloat(_progress, -0.4f);
     }
 }
